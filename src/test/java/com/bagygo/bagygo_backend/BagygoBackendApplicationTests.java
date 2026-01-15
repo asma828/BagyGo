870df610b22709fadd9104522327264440883d44
asma828 <asmalachhab66@gmail.com>
2026-01-15 10:24:48 +0100

    initialiser le projet

---
+++ src/test/java/com/bagygo/bagygo_backend/BagygoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagygo.bagygo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagygoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
